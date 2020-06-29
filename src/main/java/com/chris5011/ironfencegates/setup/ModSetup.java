b0599c8610ba28611ce5318dc146e4f0e641b8f4
Chris5011 <chris-d@gmx.at>
2020-06-29 04:48:10 +0200

    Wrote basically the whole mod and added a License (GNU GPL 3.0).
    Signed-off-by: Chris5011 <chris-d@gmx.at>

---
+++ src/main/java/com/chris5011/ironfencegates/setup/ModSetup.java
@@ -0,0 +1,10 @@
+package com.chris5011.ironfencegates.setup;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+public final class ModSetup {
+
+    public static void setup(final FMLCommonSetupEvent event) {
+
+    }
+}

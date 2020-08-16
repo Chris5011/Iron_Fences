50921e4169871d8dd4cd75bf0c0cb15c904abf2e
Chris5011 <chris-d@gmx.at>
2020-08-16 19:33:51 +0200

    - Upgraded to Forge-Beta Version 1.16.1 - 32.0.108. - The decision was made to make this version a release, although forge is still in a Beta-state. - Some Code-Cleanup was done, and the MODID of this mod was changed to "ironfences" which suits it better. - The Artifact-ID was also changed to match the domain of my Website.
    Signed-off-by: Chris5011 <chris-d@gmx.at>

---
+++ src/main/java/dev/chris5011/ironfences/setup/ClientSetup.java
@@ -0,0 +1,14 @@
+package dev.chris5011.ironfences.setup;
+
+import dev.chris5011.ironfences.IronFences;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = IronFences.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ClientSetup {
+
+    public static void clientInit(final FMLClientSetupEvent event){
+
+    }
+}
---
+++ src/main/java/dev/chris5011/ironfences/setup/Registry.java
@@ -0,0 +1,39 @@
+package dev.chris5011.ironfences.setup;
+
+import dev.chris5011.ironfences.IronFences;
+import dev.chris5011.ironfences.modules.ironfencegate.IronFenceGate_Block;
+import net.minecraft.block.Block;
+import net.minecraft.block.FenceBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
+public class Registry {
+
+    private final static DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, IronFences.MODID);
+    private final static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, IronFences.MODID);
+
+
+    public static final RegistryObject<Block> IRON_FENCE_BLOCK = BLOCKS.register("iron_fence", () -> new FenceBlock(Block.Properties.create(Material.IRON, MaterialColor.IRON).hardnessAndResistance(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Item> IRON_FENCE_BLOCK_ITEM = ITEMS.register("iron_fence", () -> new BlockItem(IRON_FENCE_BLOCK.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
+
+    public static final RegistryObject<Block> IRON_FENCE_GATE_BLOCK = BLOCKS.register("iron_fence_gate", () -> new IronFenceGate_Block(Block.Properties.create(Material.IRON, MaterialColor.IRON).hardnessAndResistance(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Item> IRON_FENCE_GATE_ITEM = ITEMS.register("iron_fence_gate", () -> new BlockItem(IRON_FENCE_GATE_BLOCK.get(), new Item.Properties().group(ItemGroup.REDSTONE)));
+
+
+    public static void registerRegistries() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(eventBus);
+        ITEMS.register(eventBus);
+    }
+
+}
